9e5bc6ae6bc728b574ae1cdef9931838f1314a11
Iago Balbino <iago_nobre@hotmail.com>
2021-07-07 09:55:34 -0300

    Adicionado a funcao que fecha o menu hamburguer quando o link e clicado

--- js/script.js
+++ js/script.js
@@ -2,10 +2,17 @@ let show = true;
 
 const menuSection = document.querySelector('.menu-section');
 const menuToggle = document.querySelector('.menu-toggle');
+const menu = document.querySelector('.menu');
 
 menuToggle.addEventListener('click', () => {
   document.body.style.overflow = show ? 'hidden' : 'initial';
 
   menuSection.classList.toggle('on', show);
   show = !show;
+});
+
+menu.addEventListener('click', () => {
+  document.body.style.overflow = 'initial';
+
+  menuSection.classList.remove('on');
 });
